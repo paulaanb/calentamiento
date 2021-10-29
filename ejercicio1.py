81e1f75b69d723d101cba8183efaec9b70fa5cb5
paulaanb <paulanb2000@gmail.com>
2021-10-29 09:25:21 +0200

    comprobacion

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,9 +2,12 @@ def bitcoinToEuros (bitcoin_amount, bitcoin_value_euros):
    euros_value = bitcoin_amount * bitcoin_value_euros
    if euros_value < 30000:
         print ("Bitcoin amount is going down")
-    else:
+    elif euros_value > 30000:
         print("Bitcoin amount is going up")
 
+    return (euros_value)
+
+def new_func(euros_value):
     return euros_value
 
 bitcoinToEuros(1,25000)
