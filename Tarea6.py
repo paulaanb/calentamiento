632c1ebc17bf92475f79c93612496d774d68c194
paulaanb <paulanb2000@gmail.com>
2021-10-30 16:30:23 +0200

    main

--- Tarea6.py
+++ Tarea6.py
@@ -10,7 +10,7 @@ else:
     else:
         shipping_cost = customer_basket_weight * shipping_cost_per_kg
         customer_cost = shipping_basket_cost + customer_basket_cost
-        print ("El precio total de su pedido asciende a la cantidad de" + str(customer_cost) + "euros")
-
+        print ("El precio total de su pedido asciende a la cantidad de" + str(customer_basket_cost) + "euros")
+        
 #Salida:¡Usted tiene envio gratis! El precio total de su pedido es 101 euros.
 #Si el valor introducido es <100 el mensaje sera el mismo mas la suma del coste envio. 
