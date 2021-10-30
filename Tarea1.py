2679e415bc5c0419159612c66f14072ce61bf3d3
paulaanb <paulanb2000@gmail.com>
2021-10-30 14:19:14 +0200

    main

--- Tarea1.py
+++ Tarea1.py
@@ -1,1 +1,5 @@
-print("Roses are #ff0000 Violets are #0000ff why my code´s working I haven´t a clue")
+import colorama
+from colorama import Fore, init
+
+colorama.init()
+print(Fore.RED + "Roses are red, " + (Fore.BLUE + "Violets are blue, ") + (Fore.WHITE + "why my code´s working I haven´t a clue"))
